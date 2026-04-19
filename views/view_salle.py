810055f879ec047e7422cdaad6c5b2c0d84df43f
raouf <300156533@monboreal.ca>
2026-04-19 16:24:51 -0400

    cc

--- views/view_salle.py
+++ views/view_salle.py
@@ -12,3 +12,20 @@ class ViewSalle(ctk.CTk):
 
         self.title("Gestion Salles")
         self.geometry("800x500")
+
+        self.frame_infos = ctk.CTkFrame(self)
+        self.frame_infos.pack()
+
+        self.code = ctk.CTkEntry(self.frame_infos)
+        self.code.pack()
+
+        self.description = ctk.CTkEntry(self.frame_infos)
+        self.description.pack()
+
+        self.categorie = ctk.CTkEntry(self.frame_infos)
+        self.categorie.pack()
+
+        self.capacite = ctk.CTkEntry(self.frame_infos)
+        self.capacite.pack()
+
+        
